28eae70688989722bcb92f0fd210ce14b4df6d00
FancyFennec <jonasallemann@gmx.ch>
2020-05-01 12:45:14 +0200

    first commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+
+    public enum EnemyState
+    {
+        IDLE,
+        SEARCHING,
+        ATTACKING
+    }
+
+    [SerializeField]
+    private Transform target;
+    public Transform head;
+    private NavMeshAgent navMeshAgent;
+
+    private readonly float fireCooldown = 2f;
+    private float fireTimer = 0f;
+
+    public EnemyState NextState { private get; set; } = EnemyState.IDLE;
+    public EnemyState CurrentState { private get; set; } = EnemyState.IDLE;
+    private Vector3 targetDirection = Vector3.zero;
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        targetDirection = target.position - head.position;
+    }
+
+    void Update()
+    {
+        targetDirection = target.position - head.position;
+        switch (CurrentState) {
+            case EnemyState.IDLE:
+                WaitForPlayer();
+                break;
+            case EnemyState.ATTACKING:
+                RotateTowardsPlayer();
+                ShootAtPlayer();
+                CheckIfPlayerVisible();
+                break;
+            case EnemyState.SEARCHING:
+                SearchPlayer();
+                break;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (CurrentState != NextState)
+        {
+            switch (NextState)
+            {
+                case EnemyState.IDLE:
+                    navMeshAgent.isStopped = false;
+                    break;
+                case EnemyState.ATTACKING:
+                    navMeshAgent.isStopped = false;
+                    break;
+                case EnemyState.SEARCHING:
+                    navMeshAgent.isStopped = false;
+                    navMeshAgent.destination = target.position;
+                    break;
+            }
+            CurrentState = NextState;
+        }
+    }
+
+    private void SearchPlayer()
+    {
+        if (IsPlayerVisible()) {
+            NextState = EnemyState.ATTACKING;
+        }
+        else
+        {
+            if ((transform.position - navMeshAgent.destination).sqrMagnitude < 0.1f)
+            {
+                NextState = EnemyState.IDLE;
+            }
+        }
+    }
+
+    private void CheckIfPlayerVisible()
+    {
+        if (!IsPlayerVisible())
+        {
+            NextState = EnemyState.SEARCHING;
+        }
+    }
+
+    private void WaitForPlayer()
+    {
+        if (IsPlayerVisible())
+        {
+            NextState = EnemyState.ATTACKING;
+        }
+    }
+
+    private void RotateTowardsPlayer()
+    {
+        Vector3 position = Vector3.Scale(targetDirection, new Vector3(1,0,1));
+        transform.rotation = Quaternion.Lerp(
+            Quaternion.LookRotation(position, Vector3.up),
+            transform.rotation,
+            0.95f);
+    }
+
+    private void ShootAtPlayer()
+    {
+        if(fireTimer == 0)
+        {
+            Debug.Log("Peng!");
+            fireTimer = fireCooldown;
+        } else
+        {
+            fireTimer = Mathf.Clamp(fireTimer - Time.deltaTime, 0f, fireCooldown);
+        }
+    }
+
+    private bool IsPlayerVisible()
+    {
+        return Vector3.Dot(targetDirection, transform.forward) > 0 &&
+                                Physics.Raycast(head.position, targetDirection, out RaycastHit hit)
+                                && hit.transform.name == "Player";
+    }
+}
---
+++ Assets/Scripts/FSPControlls.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FSPControlls : MonoBehaviour
+{
+    [Header("Input Settings")]
+    public string MouseXInput;
+    public string MouseYInput;
+    public string HorizontalInput = "Horizontal";
+    public string VerticalInput = "Vertical";
+
+    [Header("Common Settings")]
+    public float mouseSensitivity;
+    public float movementSpeed;
+
+    [Header("FPS Camera Settings")]
+    public Vector3 FPS_CameraOffset;
+    public Vector2 FPS_MinMaxAngles;
+
+    [Header("Movement Settings")]
+    public const float initialJumpMomentum = 20f;
+    public const float gravity = 50f;
+
+    CharacterController characterController;
+    Transform FPSController;
+    float xClamp;
+    bool isJumping = false;
+    float jumpMomentum = 0f;
+
+    public GameObject blood;
+
+    void Start()
+    {
+        Application.targetFrameRate = 120;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        xClamp = 0;
+        characterController = GetComponentInParent<CharacterController>();
+        FPSController = characterController.transform;
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space) && characterController.isGrounded)
+        {
+            isJumping = true;
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(
+                Camera.main.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0)), 
+                out RaycastHit hit,
+                Mathf.Infinity,
+                LayerMask.GetMask("Enemy")
+                ))
+            {
+                Vector3 incomingVec = (hit.point - transform.position).normalized;
+                Vector3 reflectVec = Vector3.Reflect(incomingVec, hit.normal);
+
+                try
+                {
+                    hit.transform.GetComponent<Rigidbody>().AddForce(transform.forward * 20f);
+                }
+                catch(MissingComponentException e) { }
+                Destroy(Instantiate(blood, hit.point, Quaternion.LookRotation(reflectVec)), 1f);
+            }
+        }
+        RotateCamera();
+        MovePlayer();
+    }
+
+    void RotateCamera()
+    {
+        Vector3 eulerRotation = transform.eulerAngles;
+        float mouseX = Input.GetAxis(MouseXInput) * (mouseSensitivity * Time.smoothDeltaTime);
+        float mouseY = Input.GetAxis(MouseYInput) * (mouseSensitivity * Time.smoothDeltaTime);
+
+        xClamp = Mathf.Clamp(xClamp + mouseY, FPS_MinMaxAngles.x, FPS_MinMaxAngles.y);
+
+        eulerRotation.x = -xClamp;
+        transform.eulerAngles = eulerRotation;
+        FPSController.Rotate(Vector3.up * mouseX);
+    }
+
+    void MovePlayer()
+    {
+        Vector3 movementVector = Vector3.zero;
+
+        float hInput = Input.GetAxisRaw(HorizontalInput);
+        float vInput = Input.GetAxisRaw(VerticalInput);
+
+        movementVector += (FPSController.forward * vInput + FPSController.right * hInput).normalized;
+        movementVector *= (Input.GetKey(KeyCode.LeftShift) ? 1.5f : 1f) * movementSpeed;
+
+        if (characterController.isGrounded)
+        {
+            if (isJumping)
+            {
+                isJumping = false;
+                jumpMomentum = initialJumpMomentum;
+            } else
+            {
+                jumpMomentum = 0f;
+            }
+        }
+        else
+        {
+            movementVector += Vector3.up * jumpMomentum;
+            jumpMomentum -= gravity * Time.smoothDeltaTime;
+        }
+        
+        characterController.Move(movementVector * Time.smoothDeltaTime);
+    }
+}
