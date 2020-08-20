b64c515850ab90cc548a68ecde3d490875d535b2
FancyFennec <jonasallemann@gmx.ch>
2020-08-20 21:48:09 +0200

    added an fps counter

---
+++ Assets/Scripts/UI/FPSCounter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class FPSCounter : MonoBehaviour
+{
+    private TextMeshProUGUI healthText;
+    void Start()
+    {
+        healthText = GetComponent<TextMeshProUGUI>();
+    }
+
+    void LateUpdate()
+    {
+        healthText.text = "fps: " + ((int) (1f / Time.deltaTime)).ToString();
+    }
+}
