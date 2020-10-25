440540caf9166829892a7e66ee0cc7b8447ba876
FancyFennec <jonasallemann@gmx.ch>
2020-10-25 18:48:35 +0100

    [weapon_swap] wrote a script that allows cars to follow paths

--- Assets/Scripts/Neutral/CarMovementController.cs
+++ Assets/Scripts/Neutral/CarMovementController.cs
@@ -4,19 +4,93 @@ using UnityEngine;
 
 public class CarMovementController : MonoBehaviour
 {
+    public GameObject target;
+    public GameObject path;
+    public bool clockwise;
 
     private Rigidbody rb;
+    private List<Transform> points = new List<Transform>();
+    private int targetIndex = 0;
+    private int nextIndex = 0;
+    private int lastIndex = 0;
+
+    private Vector3 targetDirection = Vector3.zero;
+    private Vector3 targetPosition = Vector3.zero;
+    private float distance = 0f;
+
+
 
     void Start()
     {
+        foreach(Transform child in path.transform)
+		{
+            points.Add(child);
+        }
+        
         rb = GetComponent<Rigidbody>();
     }
 
     void FixedUpdate()
     {
-        if(Vector3.Dot(transform.up, Vector3.up) > 0.8f)
+        if (distance < 0.6)
 		{
-            rb.MovePosition(transform.position + transform.forward * Time.fixedDeltaTime * 1f);
+            updateIndeces();
+            targetPosition = getTargetPosition();
+            target.transform.position = targetPosition;
+            Debug.Log(targetIndex);
+		}
+        targetDirection = (targetPosition - transform.position);
+        distance = targetDirection.magnitude;
+        targetDirection = (transform.forward + 0.03f * targetDirection.normalized).normalized;
+        if (Vector3.Dot(transform.up, Vector3.up) > 0.8f)
+		{
+            rb.MovePosition(transform.position + targetDirection * Time.fixedDeltaTime * 2f);
+        }
+        rb.MoveRotation(Quaternion.LookRotation((transform.forward + targetDirection).normalized));
+    }
+
+    private Vector3 getTargetPosition()
+	{
+        Vector3 backwards = getBackwardDirection().normalized;
+        Vector3 forwards = getForwardDirection().normalized;
+
+        Vector3 crossProd = Vector3.Cross(backwards, forwards);
+		if (crossProd.y > 0) {
+            return points[targetIndex].position - 2 * (backwards + forwards).normalized;
+        } else if(crossProd.y < 0) {
+            return points[targetIndex].position + 2 * (backwards + forwards).normalized;
+        } else {
+            return points[targetIndex].position - 2 * Vector3.Cross(forwards, Vector3.up).normalized;
         }
     }
+
+    private Vector3 getBackwardDirection()
+    {
+        return points[lastIndex].position - points[targetIndex].position;
+    }
+
+    private Vector3 getForwardDirection()
+    {
+        return points[nextIndex].position - points[targetIndex].position;
+    }
+
+    private void updateIndeces()
+	{
+        lastIndex = targetIndex;
+        targetIndex = nextIndex;
+        Crossing crossing = points[targetIndex].GetComponent<Crossing>();
+		if (crossing != null)
+		{
+			Transform nextPoint = crossing.points[UnityEngine.Random.Range(0, crossing.points.Count)];
+			while (nextPoint == points[lastIndex])
+			{
+                nextPoint = crossing.points[UnityEngine.Random.Range(0, crossing.points.Count)];
+            }
+            nextIndex = points.IndexOf(nextPoint);
+        } else
+		{
+
+            nextIndex = (targetIndex + points.Count + (clockwise ? 1 : -1)) % points.Count;
+        }
+	}
 }
---
+++ Assets/Scripts/Neutral/Crossing.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crossing : MonoBehaviour
+{
+    public List<Transform> points = new List<Transform>();
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
